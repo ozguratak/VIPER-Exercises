155f3a3aa6f8e0e5d8776862ab0a58c7c9e2d5a9
Özgür Atak <45077422+ozguratak@users.noreply.github.com>
2023-08-31 16:13:53 +0300

    Initial Commit

---
+++ Exercise 1/Exercise 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exercise 1
+//
+//  Created by Özgür  Atak  on 31.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
